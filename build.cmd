bf139b5eb111c9e3a80c9133f55a62bb8c1d3526
Korolev A.N <korolev.a.n@sblogistica.ru>
2023-06-03 15:12:31 +0300

    Add windows build script

---
+++ build.cmd
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001
+
+set PLATFORM_1C=%ProgramFiles%\1cv8\8.3.22.1750\bin
+
+rmdir /Q /S "%CD%/build/ib"
+rmdir /Q /S "%CD%/build/workspace"
+rmdir /Q /S "%CD%/build/config"
+
+mkdir build\ib
+mkdir build\cf
+
+call ring edt@2022.2.5 workspace export --workspace-location %CD%\build\workspace --configuration-files %CD%\build\config --project %CD%\CpuBenchmark
+
+"%PLATFORM_1C%\ibcmd.exe" infobase create --data "%CD%/build/ib"
+"%PLATFORM_1C%\ibcmd.exe" infobase config import --data "%CD%/build/ib" %CD%\build\config
+"%PLATFORM_1C%\ibcmd.exe" infobase config apply --data "%CD%/build/ib" --force
+"%PLATFORM_1C%\ibcmd.exe" infobase config save --data "%CD%/build/ib" "%CD%/build/cf/cpubenchmark_last.cf"
